622950dcbb3da4428aea614e7db68671fea849ce
jonathandly <dalyj138@gmail.com>
2020-01-18 12:49:28 -0500

    Moved secret key to .env, ran initial migrations, and setup api-token-auth in urls.py. Also created superuser.

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework.authtoken import views
 from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^api-token-auth/', views.obtain_auth_token),
     path('api/', include('api.urls')),
     path('api/adv/', include('adventure.urls')),
 ]
